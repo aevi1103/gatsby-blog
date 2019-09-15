b0ee22d559b8774320cbd931f40b282510aaf21e
aevi <aevirontos@gmail.com>
2019-09-14 20:01:19 -0400

    adding all our new blog code for gatsby setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,71 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
 
-export default IndexPage
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => {
+  
+  const { 
+    allMarkdownRemark : { totalCount, edges } 
+  } = data;
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <div>
+        <h1>Aevi Thoughts</h1>
+        <h4>{totalCount}</h4>
+        {
+          edges.map(({node}) => (
+            <div key={node.id}>
+              <BlogLink to={node.fields.slug}>
+                <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+              </BlogLink>
+              <p>{node.excerpt}</p>
+            </div>
+          ))
+        }
+      </div>
+
+      
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query
+    {
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order:  DESC }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              description
+              title
+              date
+            }
+            fields {
+              slug
+            }
+            html
+            excerpt
+          }
+        }
+        totalCount
+      }
+  }
+
+`
